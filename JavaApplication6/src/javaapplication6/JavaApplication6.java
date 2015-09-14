f6c201acf171a3f9c2fcc8da2944f66c29433af4
c0652847 <c0652847@SB-N208-31.lambton.on.ca>
2015-09-14 16:15:30 -0400

    Assignment1

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication6;
+
+/**
+ *
+ * @author c0652847
+ */
+public class JavaApplication6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    /** first assignment2015 
+         * Java developer--http://www.indeed.ca/cmp/TekHunter.Biz/jobs/Senior-Java-Developer-c7833cb993f81134?sjdu=vQIlM60yK_PwYat7ToXhk1EQcCGKPDd26qp_pQ6XvqP_pcp8YDdFNCmGmAtCVeR-mnfcFugo4DZm2iJOHSOQjg
+         * jdbc---database connectivity
+         * JUnit--framework to write repeatable tests 
+         * Spring---java based enterprise application
+         * jsp-- Dynamically generated web page 
+         * Toronto salary---69k
+         * Vancouver salary---82k
+         * Waterloo salary---80k
+         * */ }
+    
+}
